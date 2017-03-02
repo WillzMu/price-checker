f9a2c0e1bb3a7e3233dc5b886eb47969eddd2723
WillzMu <wilfredmulenga@yahoo.com>
2017-03-02 16:10:28 +0200

    make it better

---
+++ README.md
@@ -0,0 +1,5 @@
+#How to Provide Replies about grocery prices via Api.ai Webhook
+
+This example shows how you can use Api.ai webhook to provide different prices for a particular store.
+
+<a href="https://heroku.com/deploy" target="_blank"><img src="https://www.herokucdn.com/deploy/button.svg"></a>
---
+++ app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "check.price":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    zone = parameters.get("check.price")
+
+    cost = {'milk':10, 'bread':8, 'eggs':20}
+
+    speech = "The cost of " + zone + " is " + str(cost[zone]) + " Kwacha."
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "apiai-pricechecker"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=True, port=port, host='0.0.0.0')
