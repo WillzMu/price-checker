ef96d17cb5786dd4e89297f294d5a739c3fdec58
WillzMu <wilfredmulenga@yahoo.com>
2017-03-06 12:35:46 +0200

    make it better

--- app.py
+++ app.py
@@ -34,11 +34,9 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     zone = parameters.get("Item-Cost")
 
-    cost = {'milk':10, 'bread':8, 'eggs':20, 'sugar':11,'yogurt':13}
-    total = 0
-    for x in zone:
-        total += cost[x]
-    speech = "The cost of your goods is K"+total
+    cost = {'milk':10, 'bread':8, 'eggs':20, 'sugar':11,'yoghurt':13}
+
+    speech = "The cost of " + zone + " is " + str(cost[zone]) + " Kwacha."
 
     print("Response:")
     print(speech)
